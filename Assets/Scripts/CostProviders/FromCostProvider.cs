44f7724e29f5aa98181f3505f5c97d4865f73fab
Oleg <->
2026-05-07 23:09:58 +0300

    Minor

--- Assets/Scripts/CostProviders/FromCostProvider.cs
+++ Assets/Scripts/CostProviders/FromCostProvider.cs
@@ -2,11 +2,11 @@
 
 namespace Core.CostProviders
 {
-    public class TargetCostProvider<T> : ICostProvider<T> where T: INode<T>
+    public class FromCostProvider<T> : ICostProvider<T> where T : INode<T>
     {
         public float GetCost(T from, T to)
         {
-            return to.Weight;
+            return from.Weight;
         }
     }
 }
---
+++ Assets/Scripts/CostProviders/ToCostProvider.cs
@@ -0,0 +1,12 @@
+﻿using Core.Nodes;
+
+namespace Core.CostProviders
+{
+    public class ToCostProvider<T> : ICostProvider<T> where T: INode<T>
+    {
+        public float GetCost(T from, T to)
+        {
+            return to.Weight;
+        }
+    }
+}
