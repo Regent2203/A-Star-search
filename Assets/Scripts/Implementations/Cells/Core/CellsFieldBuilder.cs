96cd07722f9edda09f23b63dac6e54aa85def081
Oleg <->
2026-08-12 21:27:04 +0300

    Small fix

--- Assets/Scripts/Implementations/Cells/Core/CellsFieldBuilder.cs
+++ Assets/Scripts/Implementations/Cells/Core/CellsFieldBuilder.cs
@@ -1,13 +1,8 @@
 using EasyField.Fields;
 using EasyField.Fields.FieldBuilders;
-using EasyField.Implementations.Vertexes;
 using EasyField.ObjectsStorages;
 using EasyField.PathSetters;
-using EasyField.SaveSystem.Dto;
-using System.Drawing;
 using UnityEngine;
-using UnityEngine.SocialPlatforms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ScrollBar;
 
 namespace EasyField.Implementations.Cells
 {
@@ -45,10 +40,7 @@ namespace EasyField.Implementations.Cells
                 var nodePos = (Vector2)item.NodePosition;
                 var cellType = item.CellType;
 
-                var localX = nodePos.x - (size.x / 2f);
-                var localY = nodePos.y - (size.y / 2f);
-                var localPos = new Vector3(localX * _field.Grid.cellSize.x, localY * _field.Grid.cellSize.y, 0);
-                var viewPos = _field.Grid.transform.TransformPoint(localPos);
+                var viewPos = IndexToViewPos((int)nodePos.x, (int)nodePos.y, size);
 
                 _nodesBuilder.CreateItem(id, nodePos, viewPos, cellType);
             }
@@ -65,14 +57,9 @@ namespace EasyField.Implementations.Cells
             {
                 for (int y = 0; y < size.y; y++)
                 {
-                    var id = new Vector2Int(x, y);
-
-                    var localX = x - (size.x / 2f);
-                    var localY = y - (size.y / 2f);
-                    var localPos = new Vector3(localX * _field.Grid.cellSize.x, localY * _field.Grid.cellSize.y, 0);
-
-                    var nodePos = id;                    
-                    var viewPos = _field.Grid.transform.TransformPoint(localPos);
+                    var id = new Vector2Int(x, y);                    
+                    var nodePos = id;
+                    var viewPos = IndexToViewPos(x, y, size);
                     
                     _nodesBuilder.CreateItem(id, nodePos, viewPos, _config.DefaultCellType);
                 }
@@ -92,6 +79,17 @@ namespace EasyField.Implementations.Cells
             _nodeDatas.Init(size);
             _nodeViews.Init(size);
             _field.SetSize(size);
+        }
+
+        private Vector3 IndexToViewPos(int x, int y, Vector2Int size)
+        {
+            var localX = x - (size.x / 2f);
+            var localY = y - (size.y / 2f);
+            var localPos = new Vector3(localX * _field.Grid.cellSize.x, localY * _field.Grid.cellSize.y, 0);
+
+            var viewPos = _field.Grid.transform.TransformPoint(localPos);
+
+            return viewPos;
         }
     }
 }
