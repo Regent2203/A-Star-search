a2bd45dc8cbd709c1c7dd99c8953d27362911123
Oleg <->
2026-08-21 15:59:57 +0300

    Hex orientation type check

--- Assets/Scripts/Fields/HexGridField.cs
+++ Assets/Scripts/Fields/HexGridField.cs
@@ -2,6 +2,13 @@
 
 namespace EasyField.Fields
 {
+    public enum HexOrientationType
+    {
+        Undefined = 0,
+        PointyTopped = 1,
+        FlatTopped = 2,
+    }
+
     public class HexGridField : GridField
     {
         public override void SetSize(Vector2Int size)
@@ -10,6 +17,25 @@ namespace EasyField.Fields
 
             _collider.size = _grid.cellSize * new Vector2(_size.x + 0.5f, _size.y * 0.75f + 0.25f);
             _collider.offset = new Vector2(_grid.cellSize.x / 4, 0);
+        }
+
+        public HexOrientationType GetHexOrientationType()
+        {
+            if (Grid.cellLayout != GridLayout.CellLayout.Hexagon)
+            {
+                Debug.LogError($"Incorrect cell layout for HexGridField: {Grid.cellLayout}. 'Hexagon' expected.", this);
+                return HexOrientationType.Undefined;
+            }
+            switch (Grid.cellSwizzle)
+            {
+                case GridLayout.CellSwizzle.XYZ:
+                    return HexOrientationType.PointyTopped;
+                case GridLayout.CellSwizzle.YXZ:
+                    return HexOrientationType.FlatTopped;
+                default:
+                    Debug.LogError($"Incorrect cell swizzle type in grid: {Grid.cellSwizzle}. 'XYZ' or 'YXZ' expected.", this);
+                    return HexOrientationType.Undefined;
+            }
         }
     }
 }
